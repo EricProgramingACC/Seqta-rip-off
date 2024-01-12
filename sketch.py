e5dcf13336ee07b560d3e2a5145f38cb0d11e6c2
EricProgramingACC <leongairs@gmail.com>
2024-01-12 18:27:29 +0800

    until tutorial 4 (4 min) until we meet agian

--- sketch.py
+++ sketch.py
@@ -1,17 +1,37 @@
-import tkinter as tk
-from tkinter import ttk
-
-#window 
-window=tk.Tk()
-window.title('testing')
-window.geometry('500x500')
-
-#title
-title_label=ttk.Label( master=window, text = 'testing' , font='calibri 24 bold')
-title_label.pack()
+#https://www.youtube.com/watch?v=RTM9tiV_HoY
+#the new boston
+import tkinter
+from tkinter import*
 
 
+root = Tk()
+topFrame=Frame(root)
+topFrame.pack()
+bottomFrame=Frame(root)
+bottomFrame.pack(side=BOTTOM)
 
 
-#run
-window.mainloop()
+button1=Button(topFrame, text="click 1", fg="red")
+button2=Button(topFrame, text="click 2", fg="blue")
+button3=Button(topFrame, text="click 3", fg="green")
+button4=Button(bottomFrame, text="click 4", fg="purple")
+
+button1.pack(side=LEFT)
+button2.pack(side = LEFT)
+button3.pack(side = LEFT)
+button4.pack()
+
+one =Label(root, text="one", bg="red",fg="white",)
+one.pack()
+two =Label(root, text="two", bg="black",fg="white",)
+two.pack(fill=X)
+three =Label(root, text="three", bg="black",fg="grey",)
+three.pack(side=LEFT, fill=Y)
+
+lable1=Label(root, text="gay")
+lable2=Label(root, text="gay2")
+entry1=Entry(root)
+entry2=Entry(root)
+entry1.grid(row=0,column=2)
+
+root.mainloop()
